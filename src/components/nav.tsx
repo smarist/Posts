0003cf995b7839048ae902510f2331bc9670d2f3
smarist <stmarisnwokike@gmail.com>
2023-12-26 19:30:57 +0000

    feat:
    fixing the ui of the nav bar

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,9 +1,43 @@
+'use client'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
+
 const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
+    const pathname = usePathname();
+    const siteRoutes = [
+        {
+            href: './',
+            label: 'Home'
+        },
+        {
+            href: './posts',
+            label: 'Post'
+        }
+    ]
+    return (
+        <nav>
+            <ul className='flex gap-x-5 text-[14px]'>
+                {
+                    siteRoutes.map((siteRoute) => (
+                        <li key={siteRoute.href}>
+                            <Link
+                                href={siteRoute.href}
+                                className={`text-zinc-400 transition 
+                                ${pathname === siteRoute.href ?
+                                        "text-zinc-900" : ""
+                                    }`}
+                            >
+                                {siteRoute.label}
+                            </Link>
+                        </li>
+                    ))
+                }
+
+            </ul>
+        </nav>
+    )
 }
 
 export default Nav
